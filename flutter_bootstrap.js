4667a0cb2bed8c361be204df1e33ed54a5a603ef
Ashrith Reddy <ashrith.reddy180206@gmail.com>
2025-09-28 21:16:13 +0530

    Deploy Flutter web build for portfolio

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1445391440"
+    serviceWorkerVersion: "3894484178"
   }
 });
